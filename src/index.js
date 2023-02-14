c4c4fe30a012bb902254a831efdc67b55ecb43c8
ax6000 <ax6000@users.noreply.github.com>
2023-02-15 07:55:29 +0900

    2

--- src/index.js
+++ src/index.js
@@ -1,47 +1,81 @@
 var config = {
+    parent: divId,
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 400,
+    height: 300,
     scene: {
         preload: preload,
         create: create,
         update: update
+    },
+    scale: {
+        // Or set parent divId here
+        parent: divId,
+
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+
+        // Or put game size here
+        // width: 1024,
+        // height: 768,
+
+        // Minimum size
+        min: {
+            width: 400,
+            height: 300
+        },
+        // Or set minimum size like these
+        // minWidth: 800,
+        // minHeight: 600,
+
+        // Maximum size
+        max: {
+            width: 1600,
+            height: 1200
+        },
+        // Or set maximum size like these
+        // maxWidth: 1600,
+        // maxHeight: 1200,
+
+        zoom: 2,  // Size of game canvas = game size * zoom
     }
 };
 
+
 var game = new Phaser.Game(config);
+
+function loadImage(game,n,name){
+    game.load.image(name,"assets/"+n+"/"+name+".png")
+}
 
 function preload ()
 {
-    for (var i = 1; i< 27; i++){
-        // node
-        this.load.image(i+"_node","assets/"+i+"/"+i+"_node.png");
-        // left stems
-        for (var j = 1;  j < 10; j++){
-            this.load.image(i+"_l_"+j,"assets/"+i+"/"+i+"_l_"+j+".png");
-        } 
-        //right stems
-        for (j = 1;  j < 10; j++){
-            this.load.image(i+"_r_"+j,"assets/"+i+"/"+i+"_r_"+j+".png");
-        } 
-        // left leaves
-        for (j = 1;  j < 10; j++){
-            this.load.image(i+"_le_r_"+j,"assets/"+i+"/"+i+"_le_l_"+j+".png");
-        } 
-        //right leaves
-        for (j = 1;  j < 10; j++){
-            this.load.image(i+"_le_r_"+j,"assets/"+i+"/"+i+"_le_r_"+j+".png");
-        } 
-        
-    }
+    fetch("./src/parameters.json")
+    .then(response => response.json())
+    .then( data => {
+        // console.log(data.plants)
+        for(let plant in data.plants){
+            
+            loadImage(this,data.plants[plant].name,data.plants[plant].node.name)
+            for(var item in data.plants[plant].leaves){
+                loadImage(this,data.plants[plant].name,data.plants[plant].leaves[item].name)
+            }
+            for(var item in data.plants[plant].stems){
+                // console.log(data.plants[plant].stems[item])
+                loadImage(this,data.plants[plant].name,data.plants[plant].stems[item].name)
+            }
+        }
+    })
     this.load.image("soil","assets/soil.png");
     this.load.image("soil_2","assets/soil_2.png");
     this.load.image("soil_3","assets/mushroom_soil.png");
+    this.load.image("number","assets/number.png");
 }
 
 function create ()
 {
-    this.add.image(400, 200, 'soil_3');
+    // this.add.image(400, 200, 'soil_3');
+    this.add.image(10,10,"1_r_1").setScale(2,2)
 }
 
 function update ()
