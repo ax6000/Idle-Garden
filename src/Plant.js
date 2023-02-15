2cbb038c5180c4dc66f1aaada63df125dbd8ee13
ax6000 <ax6000@users.noreply.github.com>
2023-02-15 10:03:12 +0900

    index.js:45 fetch unresolved

--- src/Plant.js
+++ src/Plant.js
@@ -1,36 +1,148 @@
+const IMAGE_SIZE = new Phaser.Math.Vector2(32,64);
+const IMAGE_SCALE = 4
 class Plant{
+#position;
 #root;
+#scene;
+#species;
+#nLeaves;
+#nStems;
+    constractor(position,scene,species){
+        // this.x = x;
+        // this.y = y;
+        this.#position = position;
+        this.#scene = scene;
+        this.#species = species
+        this.#nLeaves = Object.keys(parameter.plants[species].leaves).length
+        this.#nStems= Object.keys(parameter.plants[species].stems).length
+    }
+    createRoot(){
+        this.#root = new Node(this,this.#species,this.#position) 
+    }
+    set root(node){this.#root = node;}
+    get scene(){ return this.#scene;}
+    get species(){return this.#species}
+    
 
-    constractor(x,y){
-        this.x = x;
-        this.y = y;
-    }
-    set root(node){
-        this.#root = node;
-    }
 }
 
-class Node{
-#edges = [];
+class PlantElementInfo{
+    offsetX;
+    offsetY;
+    endX;
+    endY;
+    isleft;
+    width;
+    height;
+}
+
+var PlantComponentType = {
+    Node:1,
+    Stem:2,
+    Leaf:3
+};
+
+class PlantNodeInfo extends PlantElementInfo{
+    socketOffset;
+}
+
+
+class PlantElement{
+#position;
 #img;
-    constractor(img){
-        this.img = img;
+#parent;
+#plantInfo;
+
+constractor(parent,img,position){
+    this.#parent = parent;
+    this.#position = position;
+    this.img = parent.scene.img.add(this.fixPrintPos,img).setOrigin(0, 0);
+}
+fixPrintPos(){
+    var pos = Phaser.Math.Vector2.Zero;
+    pos.x -= IMAGE_SIZE.y;
+    if (!this.#plantInfo.isleft){
+        pos.y -= IMAGE_SIZE.y;
     }
-    add(edge){
+    pos.x += this.#plantInfo.offsetX
+    pos.y += this.#plantInfo.offsetY
+    return pos * IMAGE_SCALE
+}
+loadPlantElementInfo(type,n = 0){
+    species = parent.species;
+    if (type == PlantComponentType.Node){
+        info = new PlantNodeInfo();
+        data = parameter.plants[species].node
+    }else if(type==PlantComponentType.Stem){
+        info = new PlantElementInfo();
+        data = parameter.plants[species].stems[n]
+    }else if(type == PlantComponentType.Leaf){
+        info = new PlantElementInfo();
+        data = parameter.plants[species].leaves[n]
+    }
+    info.offsetX = data.offset.x
+    info.offsetY = data.offset.y
+    info.endX = data.offset.endx
+    info.endY = data.offset.endy
+    info.height = data.size.height
+    info.width =  data.size.width
+    info.isleft =  data.size.isleft
+    if (type == PlantComponentType.Node){
+        info.socketOffset = []
+        for(var item in data.socketOffset){
+            info.socketOffset.add(Phaser.Math.Vector2(data.socketOffset[item].x,data.socketOffset[item].y))
+        }
+    }
+    return info
+}
+}
+
+class Node extends PlantElement{
+#position;
+#img;
+#parent;
+#plantInfo;
+#edges = [];
+#socketOffset = [];
+constructor(parent,img,position){
+        super.constractor(parent,img,position);
+        this.#socketOffset = this.#plantInfo.socketOffset; 
+        this.#plantInfo = this.loadPlantElementInfo(PlantComponentType.Node)
+    }
+    addEdge(edge){
         this.#edges.add(edge);
     }
 
+    
 }
 
-class Edge{
+class Stem extends PlantElement{
+    #position;
+    #parent;
+    #plantInfo;
     #nodeFrom;
     #nodeTo;
     #img;
-    constractor(from,img){
+    constractor(parent,img,position,from,n){
+        super.constractor(parent,img,position);
         this.#nodeFrom = from;
-        this.#img = img;
+        this.#plantInfo = this.loadPlantElementInfo(PlantComponentType.Stem,n)
     }
     get nodeFrom(){ return this.#nodeFrom};
     get nodeTo(){ return this.#nodeTo};
-    set nodeTo(node){this.#NodeTo=node;}
+    // set nodeTo(node){this.#NodeTo=node;}
+}
+
+class Leaf extends PlantElement{
+    #position;
+    #parent;
+    #plantInfo;
+    #nodeFrom;
+    #img;
+    constractor(parent,img,position,from,n){
+        super.constractor(parent,img,position);
+        this.#nodeFrom = from;
+        this.#plantInfo = this.loadPlantElementInfo(PlantComponentType.Leaf,n)
+    }
+    get nodeFrom(){ return this.#nodeFrom};
 }
--- src/index.js
+++ src/index.js
@@ -1,71 +1,69 @@
 var config = {
-    parent: divId,
     type: Phaser.AUTO,
-    width: 400,
-    height: 300,
+    width: 800,
+    height: 600,
     scene: {
         preload: preload,
         create: create,
         update: update
     },
-    scale: {
-        // Or set parent divId here
-        parent: divId,
+    pixelArt: true
+    // ,
+    // scale: {
+    //     parent:"game",
+    //     mode: Phaser.Scale.RESIZE,
+    //     autoCenter: Phaser.Scale.CENTER_BOTH,
 
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
+    //     // Or put game size here
+    //     // width: 1024,
+    //     // height: 768,
 
-        // Or put game size here
-        // width: 1024,
-        // height: 768,
+    //     // Minimum size
+    //     min: {
+    //         width: 400,
+    //         height: 300
+    //     },
+    //     // Or set minimum size like these
+    //     // minWidth: 800,
+    //     // minHeight: 600,
 
-        // Minimum size
-        min: {
-            width: 400,
-            height: 300
-        },
-        // Or set minimum size like these
-        // minWidth: 800,
-        // minHeight: 600,
+    //     // Maximum size
+    //     max: {
+    //         width: 1600,
+    //         height: 1200
+    //     },
+    //     // Or set maximum size like these
+    //     // maxWidth: 1600,
+    //     // maxHeight: 1200,
 
-        // Maximum size
-        max: {
-            width: 1600,
-            height: 1200
-        },
-        // Or set maximum size like these
-        // maxWidth: 1600,
-        // maxHeight: 1200,
-
-        zoom: 2,  // Size of game canvas = game size * zoom
-    }
+    //     zoom: 2,  // Size of game canvas = game size * zoom
+    // }
 };
 
 
 var game = new Phaser.Game(config);
-
+var parameter = fetch("./src/parameters.json").then(response => response.json())
 function loadImage(game,n,name){
     game.load.image(name,"assets/"+n+"/"+name+".png")
 }
 
 function preload ()
 {
-    fetch("./src/parameters.json")
-    .then(response => response.json())
-    .then( data => {
-        // console.log(data.plants)
-        for(let plant in data.plants){
-            
-            loadImage(this,data.plants[plant].name,data.plants[plant].node.name)
-            for(var item in data.plants[plant].leaves){
-                loadImage(this,data.plants[plant].name,data.plants[plant].leaves[item].name)
-            }
-            for(var item in data.plants[plant].stems){
-                // console.log(data.plants[plant].stems[item])
-                loadImage(this,data.plants[plant].name,data.plants[plant].stems[item].name)
-            }
+    this.load.script('plant', 'src/Plant.js');
+    var data = parameter;
+        console.log(data.plants)
+    for(let plant in data.plants){
+        
+        loadImage(this,data.plants[plant].name,data.plants[plant].node.name)
+        for(var item in data.plants[plant].leaves){
+            loadImage(this,data.plants[plant].name,data.plants[plant].leaves[item].name)
         }
-    })
+        for(var item in data.plants[plant].stems){
+            // console.log(data.plants[plant].stems[item])
+            loadImage(this,data.plants[plant].name,data.plants[plant].stems[item].name)
+        }
+    }
+    
     this.load.image("soil","assets/soil.png");
     this.load.image("soil_2","assets/soil_2.png");
     this.load.image("soil_3","assets/mushroom_soil.png");
@@ -75,7 +73,8 @@ function preload ()
 function create ()
 {
     // this.add.image(400, 200, 'soil_3');
-    this.add.image(10,10,"1_r_1").setScale(2,2)
+    this.add.image(0,0,"1_r_1").setScale(1,1).setOrigin(0,0)
+    // var Plant1 = new Plant(Phaser.Math.Vector2.Zero,this,1)
 }
 
 function update ()
